9f1118886fff69a9dd0f19b6ac4e20c2ee5b1457
Akshada-khele <98120017+Akshada-khele@users.noreply.github.com>
2022-01-21 00:40:25 +0530

    uploaded GATE_AND

---
+++ GATE_AND.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.12.2021 17:39:28
+// Design Name: 
+// Module Name: GATE_AND
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module GATE_AND(
+    input A,
+    input B,
+    output Y
+    );
+    and(Y,A,B);
+    
+endmodule
